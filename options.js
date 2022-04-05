52ffa6fa9e54ac888f8b6c23173dd8b484a359ab
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-06 00:12:54 +0900

    implement chrome api to save data

--- options.js
+++ options.js
@@ -3,5 +3,17 @@ const nameInput = document.getElementById("name-input");
 const saveBtn = document.getElementById("save-btn");
 
 saveBtn.addEventListener("click", () => {
-  console.log(nameInput.value);
+  const name = nameInput.value;
+  chrome.storage.sync.set(
+    {
+      name,
+    },
+    () => {
+      console.log(`Name is set to ${name}`);
+    }
+  );
+});
+
+chrome.storage.sync.get(["name", "test"], (res) => {
+  nameInput.value = res.name;
 });
--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 const timeElement = document.getElementById("time");
+const nameElement = document.getElementById("name");
 const currentTime = new Date().toLocaleTimeString();
 
 timeElement.textContent = `The time is: ${currentTime}`;
@@ -11,3 +12,8 @@ chrome.action.setBadgeText(
     console.log("Finished setting badge text");
   }
 );
+
+chrome.storage.sync.get(["name"], (res) => {
+  const name = res.name ?? "anonymous";
+  nameElement.textContent = `Hi ${name}!`;
+});
