f1640e3f8b6f75c6ebef27767d4b4ecfd6175cf3
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-05 23:57:11 +0900

    add options page

---
+++ options.js
@@ -0,0 +1,7 @@
+const nameInput = document.getElementById("name-input");
+
+const saveBtn = document.getElementById("save-btn");
+
+saveBtn.addEventListener("click", () => {
+  console.log(nameInput.value);
+});
