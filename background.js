23e8759b2e27fa239e3a0595776e1b868c16e23d
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-06 20:27:41 +0900

    set notification time functionality

--- background.js
+++ background.js
@@ -14,12 +14,15 @@ chrome.alarms.onAlarm.addListener(() => {
       text: `${time + 1}`,
     });
 
-    if (time % 1000 === 0) {
-      this.registration.showNotification("Chrome Timer Extension", {
-        body: "1000 seconds has passed!",
-        icon: "icon.png",
-      });
-    }
+    chrome.storage.sync.get(["notificationTime"], (res) => {
+      const notificationTime = res.notificationTime ?? 1000;
+      if (time % notificationTime == 0) {
+        this.registration.showNotification("Chrome Timer Extension", {
+          body: `${notificationTime} seconds has passed.`,
+          icon: "icon.png",
+        });
+      }
+    });
   });
   //   console.log(alarm);
 });
