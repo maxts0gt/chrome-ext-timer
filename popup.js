aaa39d6143fba77d5571cb5d4c007ef3aa28e13e
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-06 20:35:08 +0900

    enable stop start reset timer functionality;

--- popup.js
+++ popup.js
@@ -1,6 +1,28 @@
 const timeElement = document.getElementById("time");
 const nameElement = document.getElementById("name");
 const timerElement = document.getElementById("timer");
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
+
+startBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: true,
+  });
+});
+
+stopBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: false,
+  });
+});
+
+resetBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    timer: 0,
+    isRunning: false,
+  });
+});
 
 function updateTimeElements() {
   chrome.storage.local.get(["timer"], (res) => {
